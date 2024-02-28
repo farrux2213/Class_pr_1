0063b3137b870db851577e2a7b2a7b4dd2c9ffbd
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-28 21:39:24 +0500

    person 2

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ class Odom {
   }
 }
 
-var Farrukh = new Odom("Farrukh", "Asqaraliyev", "Tashkent", "19");
+class Odom_ikki {
+  constructor(name, surname, adress, age, nameCar) {
+    this.name = name;
+    this.surname = surname;
+    this.adress = adress;
+    this.age = age;
+    this.nameCar = nameCar;
+  }
+}
 
-console.log(Farrukh);
+var Farrukh = new Odom("Farrukh", "Asqaraliyev", "Tashkent", "19");
+var Umar = new Odom_ikki("Umar", "Saidmuradov", "Toraqorgon", "18", "Jentra");
+// console.log(Farrukh);
+console.log(Umar);
 
 //DRY -> don't repeat yourself
 //KISS -> keep it super simple
