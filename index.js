01259b0d2f269cbe21c4c5bdc020d61a3e06b3f6
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-28 21:49:51 +0500

    inherted person three

--- index.js
+++ index.js
@@ -14,10 +14,25 @@ class Odom_ikki extends Odom {
   }
 }
 
+class Odom_uch extends Odom {
+  constructor(name, surname, adress, age, nameCar) {
+    super(name, surname, adress, age);
+    this.nameCar = nameCar;
+  }
+}
+
 var Farrukh = new Odom("Farrukh", "Asqaraliyev", "Tashkent", "19");
 var Umar = new Odom_ikki("Umar", "Saidmuradov", "Toraqorgon", "18", "Jentra");
+var Shaxboz = new Odom_uch(
+  "Shaxbozbek",
+  "Joramirzayev",
+  "Namangan",
+  "18",
+  "Nexia 1"
+);
 // console.log(Farrukh);
-console.log(Umar);
+// console.log(Umar);
+console.log(Shaxboz);
 
 //DRY -> don't repeat yourself
 //KISS -> keep it super simple
