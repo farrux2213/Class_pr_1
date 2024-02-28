fc1b97f4bd6013fa31cc9f79aa7240eeec747e33
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-28 22:12:07 +0500

    all commits

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-class Odom {
-  constructor(name, surname, adress, age) {
-    this.name = name;
-    this.surname = surname;
-    this.adress = adress;
-    this.age = age;
-    this.fullName = function () {
-      return `${this.name} ${this.surname}`;
-    };
-  }
-}
+// class Odom {
+//   constructor(name, surname, adress, age) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.adress = adress;
+//     this.age = age;
+//     this.fullName = function () {
+//       return `${this.name} ${this.surname}`;
+//     };
+//   }
+// }
 
-class Odom_ikki extends Odom {
-  constructor(name, surname, adress, age, nameCar) {
-    super(name, surname, adress, age);
-    this.nameCar = nameCar;
-  }
-}
+// class Odom_ikki extends Odom {
+//   constructor(name, surname, adress, age, nameCar) {
+//     super(name, surname, adress, age);
+//     this.nameCar = nameCar;
+//   }
+// }
 
-class Odom_uch extends Odom {
-  constructor(name, surname, adress, age, nameCar) {
-    super(name, surname, adress, age);
-    this.nameCar = nameCar;
-  }
-}
+// class Odom_uch extends Odom {
+//   constructor(name, surname, adress, age, nameCar) {
+//     super(name, surname, adress, age);
+//     this.nameCar = nameCar;
+//   }
+// }
 
-var Farrukh = new Odom("Farrukh", "Asqaraliyev", "Tashkent", "19");
-var Umar = new Odom_ikki("Umar", "Saidmuradov", "Toraqorgon", "18", "Jentra");
-var Shaxboz = new Odom_uch(
-  "Shaxbozbek",
-  "Joramirzayev",
-  "Namangan",
-  "18",
-  "Nexia 1"
-);
+// var Farrukh = new Odom("Farrukh", "Asqaraliyev", "Tashkent", "19");
+// var Umar = new Odom_ikki("Umar", "Saidmuradov", "Toraqorgon", "18", "Jentra");
+// var Shaxboz = new Odom_uch(
+//   "Shaxbozbek",
+//   "Joramirzayev",
+//   "Namangan",
+//   "18",
+//   "Nexia 1"
+// );
 // console.log(Farrukh);
 // console.log(Umar);
 // console.log(Shaxboz.fullName());
